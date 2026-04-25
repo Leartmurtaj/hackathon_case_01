7b185a30ffdb4c268b94212757db0ac4f4276cea
Yanis Länzlinger <yanis.laenzlinger@students.bfh.ch>
2026-04-25 21:37:07 +0200

    fix QR Code copy btn

--- hackathon/src/main/webapp/js/components/wallet.js
+++ hackathon/src/main/webapp/js/components/wallet.js
@@ -87,15 +87,17 @@ export default {
                         -->
 
                         <div class="qr-container">
-                            <div class="qr">
+                        <div class="qr-image-box">
                             <img src="${item.benefit.qrCode}" width="220" height="220" alt="QR Code">
-                                <div><strong>${item.benefit.qrToken}</strong></div>
-                                </div>
-                            <button class="icon-btn copy-btn" title="Copy code" data-code="${item.benefit.qrCode}">
+                        </div>
+                        <div class="qr-token-box">
+                            <strong>${item.benefit.qrToken}</strong>
+                            <button class="icon-btn copy-btn" title="Copy code" data-code="${item.benefit.qrToken}">
                                 <svg viewBox="0 0 24 24">
                                     <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
                                 </svg>
                             </button>
+                        </div>
                         </div>
 
                         <p class="meta mb-0">
